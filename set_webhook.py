62e192226d3f410f0a5a5ac66b2887d03981b532
MR.ExceFaN <106883384+fanky86@users.noreply.github.com>
2024-12-18 06:31:11 +0700

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -4,7 +4,8 @@ import requests
 TELEGRAM_TOKEN = '7829625950:AAHAkDANqB9yalb2vClpxX5zXBpHBaq_iVM'
 
 # Ganti dengan URL webhook aplikasi yang sudah dideploy di Vercel
-WEBHOOK_URL = 'https://bottelegram-two.vercel.app/webhook'  # URL Vercel Anda
+WEBHOOK_URL = 'https://bottelegram-two.vercel.app/webhook'  # URL aplikasi Anda di Vercel
+
 # Mengatur webhook Telegram
 url = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/setWebhook?url={WEBHOOK_URL}'
 
